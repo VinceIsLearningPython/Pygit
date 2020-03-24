7c8525afc871eb174d6dce67519e6d9b167ebaab
VinceIsLearningPython <62544033+VinceIsLearningPython@users.noreply.github.com>
2020-03-24 20:13:59 +0800

    Update py.py

--- py.py
+++ py.py
@@ -4,10 +4,12 @@ def prinput(a='',c='',b='.'):
     input(b)
 print('Hello')
 input("to continue,press ENTER .")
-a = input("Good Afternoon. If You Are A Developer I Asked For Help On Facebook Enter The Password. ->")
-if a = "P@SSwørd":
+a = input("Good Afternoon. If You Are A Developer I Asked For Help On Facebook Enter The Password There. ->")
+if a == Devpass:
   input("Welcome,My Dear Friend Of Mine.Would You Like A Special Route That Is Only For Devs?  Yes Or No?")
   devspeacial()
+if a == passwordyusi:
+    print("Hello Yusi.")
 Confirm = prinput('So You Like',b,"?")
 if Confirm = "Yes":
   
